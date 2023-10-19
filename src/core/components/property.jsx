cc4097710dca73f0d634e19c399851135660d6a9
Yi Sun <yi.sun@konghq.com>
2023-10-19 16:50:36 +0800

    feat: backport info properties support for `<ObjectModel />` from upstream
    This fix FTI-5480

--- src/core/components/property.jsx
+++ src/core/components/property.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-export const Property = ({ propKey, propVal, propStyle }) => {
+export const Property = ({ propKey, propVal, propStyle, propClass }) => {
     return (
-        <span style={ propStyle }>
+        <span style={ propStyle } className={ propClass }>
           <br />{ propKey }: { String(propVal) }</span>
     )
 }
 Property.propTypes = {
   propKey: PropTypes.string,
   propVal: PropTypes.any,
-  propStyle: PropTypes.object
+  propStyle: PropTypes.object,
+  propClass: PropTypes.string,
 }
 
 export default Property
